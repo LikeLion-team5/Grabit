30a23b4e44e476e64182578364c2105612e21cc8
heeyeon3050 <kho200a@naver.com>
2023-05-25 13:13:01 +0900

    세팅

---
+++ src/main/java/com/ll/grabit/GrabitApplication.java
@@ -0,0 +1,13 @@
+package com.ll.grabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabitApplication.class, args);
+	}
+
+}
