802d978813408e1fce6d58a3c5adbfcf22a92198
Choi-Hyeok99 <gur0709@naver.com>
2023-05-30 17:45:28 +0900

    CSRF 기능 해제

--- src/main/java/com/ll/grabit/base/security/SecurityConfig.java
+++ src/main/java/com/ll/grabit/base/security/SecurityConfig.java
@@ -14,18 +14,18 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//                .authorizeHttpRequests(
-//                        authorize -> authorize
-//                                .anyRequest().permitAll()
-//                ).csrf(
-//                /*
-//                식당 등록 테스트를 위해서 csrf 토큰 꺼둔다. POST, PUT, DELETE 같은
-//                데이터 변경이 일어날만한 HTTP 메서드에서는 csrf 토큰을 함께 받아야지 실행이 된다.
-//                따라서 원할한 테스트를 위해서 꺼둔다.
-//                 */
-//                        csrfConfigurer -> csrfConfigurer
-//                                .disable()
-//                )
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .anyRequest().permitAll()
+                ).csrf(
+                /*
+                식당 등록 테스트를 위해서 csrf 토큰 꺼둔다. POST, PUT, DELETE 같은
+                데이터 변경이 일어날만한 HTTP 메서드에서는 csrf 토큰을 함께 받아야지 실행이 된다.
+                따라서 원할한 테스트를 위해서 꺼둔다.
+                 */
+                        csrfConfigurer -> csrfConfigurer
+                                .disable()
+                )
                 .formLogin(
                         formLogin -> formLogin
                                 .loginPage("/member/login")
