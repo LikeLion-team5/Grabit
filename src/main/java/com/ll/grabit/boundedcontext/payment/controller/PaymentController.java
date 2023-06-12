b0e1e4e64a7818d91b6edbe004889c4fba103a7e
Kminwoo97 <kmo971130@gmail.com>
2023-06-12 22:19:57 +0900

    Feat: 예약버튼 누르면 결제및 예약기능 연동

--- src/main/java/com/ll/grabit/boundedcontext/payment/controller/PaymentController.java
+++ src/main/java/com/ll/grabit/boundedcontext/payment/controller/PaymentController.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.grabit.base.rq.Rq;
+import com.ll.grabit.base.rsdata.RsData;
 import com.ll.grabit.boundedcontext.member.entity.Member;
 import com.ll.grabit.boundedcontext.payment.exception.AmountNotMatchedException;
 import com.ll.grabit.boundedcontext.payment.service.PaymentService;
+import com.ll.grabit.boundedcontext.reservation.dto.ReservationRequestDto;
+import com.ll.grabit.boundedcontext.reservation.service.ReservationService;
 import com.ll.grabit.boundedcontext.restaurant.entity.Restaurant;
 import com.ll.grabit.boundedcontext.restaurant.service.RestaurantService;
 import com.nimbusds.jose.shaded.gson.JsonObject;
@@ -15,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import net.minidev.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,23 +28,30 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/payment")
 public class PaymentController {
 
     private final RestaurantService restaurantService;
-    private final PaymentService paymentService;
+
+    private final ReservationService reservationService;
+
     private final Rq rq;
+
+    private final PaymentService paymentService;
 
     private final String SECRET_KEY = "test_sk_7XZYkKL4Mrjg6q2bvqm80zJwlEWR";
     private final RestTemplate restTemplate = new RestTemplate();
     private final ObjectMapper objectMapper;
 
-    @GetMapping("{restaurantId}/payment")
+    @GetMapping("/{restaurantId}/payment")
     public String paymentPage(@PathVariable Long restaurantId, Model model) {
         Restaurant restaurant = restaurantService.findOne(restaurantId);
         Member member = rq.getMember();
@@ -64,14 +75,19 @@ public class PaymentController {
         });
     }
 
-    @GetMapping("{restaurantId}/success")
+    @GetMapping("/{restaurantId}/pay/success")
+    @PreAuthorize("isAuthenticated() and hasAuthority('member')")
     public String success(
             @PathVariable Long restaurantId,
             @RequestParam String paymentKey,
             @RequestParam String orderId,
-            @RequestParam Integer amount,
+            @RequestParam Integer amount, //여기까지는 필수 정보
+            @RequestParam String name,
+            @RequestParam String phone,
+            @RequestParam LocalDate date,
+            @RequestParam String reservationTime,
+            @RequestParam Integer partySize,
             Model model) throws JsonProcessingException {
-
         if (amount < 0 || amount != 10000) {
             throw new AmountNotMatchedException();
         }
@@ -82,21 +98,36 @@ public class PaymentController {
         headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
         headers.setContentType(MediaType.APPLICATION_JSON);
 
+        //페이로드 설정 후, request 생성
         Map<String, String> payloadMap = new HashMap<>();
         payloadMap.put("orderId", orderId);
         payloadMap.put("amount", String.valueOf(amount));
-
         HttpEntity<String> request = new HttpEntity<>(objectMapper.writeValueAsString(payloadMap), headers);
 
+        //토스 페이먼츠 결제 요청
         ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(
                 "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
 
 
+        //결제 성공
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             Member member = rq.getMember();
             paymentService.save(member, paymentKey, orderId, amount);
 
-            return "toss/success";
+            Member loginMember = rq.getMember();
+            if (loginMember != null && restaurantId != null) {
+                ReservationRequestDto reservationRequestDto = new ReservationRequestDto();
+                reservationRequestDto.setName(name);
+                reservationRequestDto.setPhone(phone);
+                reservationRequestDto.setDate(date);
+                reservationRequestDto.setReservationTime(extractedLocalTime(reservationTime));
+                reservationRequestDto.setPartySize(partySize);
+                reservationRequestDto.setRestaurantId(restaurantId);
+                reservationRequestDto.setMemberId(loginMember.getId());
+                reservationService.createReservation(reservationRequestDto);
+            }
+
+            return "redirect:/payment/success";
         } else {
             //결제 실패
             JsonNode failNode = responseEntity.getBody();
@@ -106,15 +137,27 @@ public class PaymentController {
         }
     }
 
-    @GetMapping("{restaurantId}/fail")
+    @GetMapping("/{restaurantId}/pay/fail")
+    @PreAuthorize("isAuthenticated() and hasAuthority('member')")
     public String failPayment(@PathVariable Long restaurantId, @RequestParam String message, @RequestParam String code, Model model) {
         model.addAttribute("message", message);
         model.addAttribute("code", code);
         return "toss/fail";
     }
 
+    //결제 성공 시, 결제 성공 알림 페이지
+    @PreAuthorize("isAuthenticated() and hasAuthority('member')")
+    @GetMapping("/success")
+    public String successPage(){
+        return "toss/success";
+    }
 
+
+
+
+    //결제 취소 API
     @PostMapping("/cancel")
+    @PreAuthorize("isAuthenticated() and hasAuthority('member')")
     @ResponseBody
     public ResponseEntity<String> requestPaymentCancel(@RequestParam String paymentKey,
                                        @RequestParam String cancelReason){
@@ -122,8 +165,6 @@ public class PaymentController {
 
         URI uri = URI.create("https://api.tosspayments.com/v1/payments/" + paymentKey + "/cancel");
         HttpHeaders headers = new HttpHeaders();
-//        byte[] secretKeyByte = (SECRET_KEY + ":").getBytes(StandardCharsets.UTF_8);
-//        headers.setBasicAuth(new String(Base64.getEncoder().encode(secretKeyByte)));
         headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -134,5 +175,11 @@ public class PaymentController {
         String response = restTemplate1.postForObject(uri, new HttpEntity<>(param, headers), String.class);
         return ResponseEntity.ok(response);
 
+    }
+
+    private static LocalTime extractedLocalTime(String time) {
+        String[] split = time.split(":");
+        LocalTime localTime = LocalTime.of(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+        return localTime;
     }
 }
--- src/main/java/com/ll/grabit/boundedcontext/reservation/controller/ReservationController.java
+++ src/main/java/com/ll/grabit/boundedcontext/reservation/controller/ReservationController.java
@@ -44,7 +44,6 @@ public class ReservationController {
     @ResponseBody
     @Transactional
     public RsData<Long> createReservation(@RequestBody ReservationRequestDto reservationDto) {
-        System.out.println();
         if (rq.isLogin()) {
             String loggedInUserId = rq.getMember().getUsername();
             Member loggedInMember = memberRepository.findByUsername(loggedInUserId).orElse(null);
