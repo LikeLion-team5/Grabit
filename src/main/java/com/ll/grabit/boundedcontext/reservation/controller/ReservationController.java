ad0e8c6fd757d2febd9c90f1ba9cb0e487d330d6
seongmin8636 <moss3004@gmail.com>
2023-06-13 21:13:12 +0900

    Feat : 예약날짜가 가까운 순으로 기본 정렬, 먼 순으로도 정렬 가능

--- src/main/java/com/ll/grabit/boundedcontext/reservation/controller/ReservationController.java
+++ src/main/java/com/ll/grabit/boundedcontext/reservation/controller/ReservationController.java
@@ -18,6 +18,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,12 +64,22 @@ public class ReservationController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/check")
-    public String showReservationCheckPage(Model model) {
+    public String showReservationCheckPage(Model model, @RequestParam(required = false) String sort) {
         if (rq.isLogin()) {
             String loggedInUserId = rq.getMember().getUsername();
             Member loggedInMember = memberRepository.findByUsername(loggedInUserId).orElse(null);
             if (loggedInMember != null) {
                 List<ReservationResponseDto> reservationList = reservationService.getReservationsByMemberId(loggedInMember.getId());
+
+                // 선택한 정렬 옵션에 따라 목록을 정렬
+                if ("near".equals(sort)) {
+                    // 예약 날짜가 가까운 순
+                    reservationList.sort(Comparator.comparing(ReservationResponseDto::getDate).thenComparing(ReservationResponseDto::getReservationTime));
+                } else if ("far".equals(sort)) {
+                    // 예약 날짜가 먼 순
+                    reservationList.sort(Comparator.comparing(ReservationResponseDto::getDate).thenComparing(ReservationResponseDto::getReservationTime).reversed());
+                }
+
                 model.addAttribute("reservations", reservationList);
             }
         }
