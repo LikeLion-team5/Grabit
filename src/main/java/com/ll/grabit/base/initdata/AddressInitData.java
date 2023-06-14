c5b2f230d21d55956bfa9be3008310cc8cbd1ffd
Kminwoo97 <kmo971130@gmail.com>
2023-06-14 17:01:07 +0900

    Fix: 배포를 고려해 테스트 데이터  배포에서 제외

--- src/main/java/com/ll/grabit/base/initdata/AddressInitData.java
+++ src/main/java/com/ll/grabit/base/initdata/AddressInitData.java
@@ -5,6 +5,7 @@ import com.ll.grabit.boundedcontext.address.repository.AddressRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.BufferedReader;
@@ -13,6 +14,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 @Configuration
+@Profile({"dev", "test"})
 public class AddressInitData {
     @Bean
     CommandLineRunner initAddressData(AddressRepository addressRepository) {
